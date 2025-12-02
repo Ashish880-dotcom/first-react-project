df705956f5c852718b3104287e946bba0d658778
Ashish Chaudhary <chaudharyaashish002@gmail.com>
2025-12-02 16:48:54 +0545

    react1

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+
+function App() {
+  var b=10;
+
+  return ( <>
+   Hello   World
+   <p>
+    Hello I am Ashish Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae recusandae dignissimos laudantium non? Nihil ullam labore quae adipisci explicabo amet nobis at voluptates error illum repudiandae magnam, corporis eos provident?
+   </p>
+   <h1>This is my first react project</h1>
+   <div>
+    {b}
+   </div>
+   <input type="text"></input>
+   </>
+
+  )   
+}
+
+export default App
