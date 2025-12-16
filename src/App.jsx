1bcbe421acd28a35b5a83f85bb7fad77a8f3796b
Ashish Chaudhary <chaudharyaashish002@gmail.com>
2025-12-16 11:44:12 +0545

    feat: implement ThemeProvider and ThemeContext for theme management

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,12 @@
-import { useRandomDog } from "./Hooks/useRandomDog";
+import React from 'react'
+import ThemeProvider from './context/ThemeProvider'
 
 const App = () => {
-  const { randomDog, handleGenerateNewImage } = useRandomDog();
   return (
-    <div className="flex h-screen flex-col items-center gap-10">
-      <div className="text-3xl font-bold">Random Dog Generator</div>
-      <button className="text-2xl italic border p-3 cursor-pointer" onClick={handleGenerateNewImage}>Generate</button>
-      {randomDog?.message ? (
-        <img src={randomDog.message} alt="Random Dog" className="max-w-xs rounded shadow" />
-      ) : (
-        <div className="text-lg">Loading...</div>
-      )}
-    </div>
-  );
-};
+   <ThemeProvider>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptate laboriosam, cumque dignissimos, illum assumenda labore dolore nihil esse repudiandae temporibus consectetur repellat necessitatibus mollitia ullam eaque? Ea, eius unde? Quos!
+   </ThemeProvider>
+  )
+}
 
-export default App;
+export default App
